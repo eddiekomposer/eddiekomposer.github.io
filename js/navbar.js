41c95ed309cce8d79fc2b5b9d2787917770fd7e6
Eddie Ko <71243941+eddiekomposer@users.noreply.github.com>
2023-07-29 20:39:57 -0700

    7/29/2023

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector('.navbar').classList.remove('hidden');
+    } else {
+      document.querySelector('.navbar').classList.add('hidden');
+    }
+    prevScrollpos = currentScrollPos;
+  }
+});
