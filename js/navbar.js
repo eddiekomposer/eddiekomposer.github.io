a90494ad94b11c1eea0526ac274d929ba9920e13
Eddie Ko <71243941+eddiekomposer@users.noreply.github.com>
2023-08-04 20:46:37 -0700

    navbar fixed

--- js/navbar.js
+++ js/navbar.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
+    if(currentScrollPos <= 0){
+       document.querySelector('.navbar').classList.remove('hidden');
+       return;
+    }
     if (prevScrollpos > currentScrollPos) {
+      console.log('show',window.pageYOffset);
       document.querySelector('.navbar').classList.remove('hidden');
     } else {
+      console.log('hidden',window.pageYOffset);
       document.querySelector('.navbar').classList.add('hidden');
     }
     prevScrollpos = currentScrollPos;
